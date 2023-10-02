d79c14939a1ed0cf6b3f1a1f92e7e114d1009e00
D.J. Morvay <djmorvay@me.com>
2023-10-02 10:54:00 -0400

    More helpers for packing headers.

--- Communications/Packet Simulation/Helpers/header.h
+++ Communications/Packet Simulation/Helpers/header.h
@@ -3,6 +3,7 @@
 #ifndef UNPACK
 #define UNPACK
 
+/* ---------- Header Struct ---------- */
 typedef struct spp_header {
     uint8_t header_array[6];
     uint8_t version_number;
@@ -14,32 +15,119 @@ typedef struct spp_header {
     uint16_t data_length;
 } header;
 
+/* ---------- Getter functions ---------- */
+/** Function: get_version_number
+ *  Address Info: Byte 0, Bits 0-2
+ */
 uint8_t get_version_number(uint8_t array[6]) {
     return (array[0] >> 5) & (uint8_t)(7);
 }
 
+/** Function: get_packet_type
+ *  Address Info: Byte 0, Bit 3
+ */
 uint8_t get_packet_type(uint8_t array[6]) {
     return (array[0] >> 4) & (uint8_t)(1);
 }
 
+/** Function: get_secondary_header
+ *  Address Info: Byte 0, Bit 4
+ */
 uint8_t get_sec_header_flag(uint8_t array[6]) {
     return (array[0] >> 3) & (uint8_t)(1);
 }
 
+/** Function: get_APID
+ *  Address Info: 
+ *      MSB - Byte 0, Bits 5-7
+ *      LSB - Byte 1, Bits 0-7
+ */
 uint16_t get_APID(uint8_t array[6]) {
     return (((uint16_t)(array[0]) & (uint16_t)(7)) << 8) + array[1];
 }
 
+/** Function: get_sequence_flag
+ *  Address Info: Byte 2, Bits 0-1
+ */
 uint8_t get_sequence_flag(uint8_t array[6]) {
     return (array[2] >> 6) & (uint8_t)(3);
 }
 
+/** Function: get_sequence_count
+ *  Address Info: 
+ *      MSB - Byte 2, Bits 2-7
+ *      LSB - Byte 3, Bits 0-7
+ */
 uint16_t get_sequence_count(uint8_t array[6]) {
     return (((uint16_t)(array[2]) & (uint16_t)(63)) << 8) + array[3];
 }
 
+/** Function: get_data_length
+ *  Address Info: 
+ *      MSB - Byte 4, Bits 0-7
+ *      LSB - Byte 5, Bits 0-7
+ */
 uint16_t get_data_length(uint8_t array[6]) {
     return ((uint16_t)(array[4]) << 8) + array[5];
+}
+
+/* ---------- Setter functions ---------- */
+/** Function: set_version_number
+ *  Address Info: Byte 0, Bits 0-2
+ */
+void set_version_number(uint8_t version_number, uint8_t *array) {
+    array[0] = ((array[0] & (uint8_t)0b00011111) | (version_number << 5));
+} 
+
+/** Function: set_packet_type
+ *  Address Info: Byte 0, Bits 3
+ */
+void set_packet_type(uint8_t packet_type, uint8_t *array) {
+    array[0] = ((array[0] & (uint8_t)0b11101111) | (packet_type << 4));
+} 
+
+/** Function: set_secondary_header
+ *  Address Info: Byte 0, Bit 4
+ */
+void set_sec_header_flag(uint8_t sec_header_flag, uint8_t *array) {
+    array[0] = ((array[0] & (uint8_t)0b11110111) | (sec_header_flag << 3));
+}
+
+/** Function: set_APID
+*  Address Info: 
+ *      MSB - Byte 0, Bits 5-7
+ *      LSB - Byte 1, Bits 0-7
+ */ 
+void set_APID(uint16_t APID, uint8_t *array) {
+    array[0] = ((array[0] & (uint8_t)0b11111000) | (uint8_t)((APID >> 8) & 0b00000111));
+    array[1] = (uint8_t)(APID & 0b11111111);
+}
+
+/** Function: set_sequence_flag
+*   Address Info: Byte 2, Bits 0-1
+ */ 
+void set_sequence_flag(uint8_t sequence_flag, uint8_t *array) {
+    array[2] = ((array[2] & (uint8_t)0b00111111) | (sequence_flag << 6));
+}
+
+/** Function: set_sequence_count
+ *  Address Info: 
+ *      MSB - Byte 2, Bits 2-7
+ *      LSB - Byte 3, Bits 0-7
+*/
+void set_sequence_count(uint16_t sequence_count, uint8_t *array) {
+    array[2] = ((array[2] & (uint8_t)0b11000000) | (uint8_t)((sequence_count >> 8) & 0b00111111));
+    array[3] = (uint8_t)(sequence_count & 0b11111111);
+}
+
+/** Function: set_data_length
+ *  Address Info: 
+ *      MSB - Byte 4, Bits 0-7
+ *      LSB - Byte 5, Bits 0-7
+*/
+void set_data_length(uint16_t data_length, uint8_t *array) {
+    array[4] = (uint8_t)((data_length >> 8) & 0b11111111);
+    array[5] = (uint8_t)(data_length & 0b11111111);
 }
 
 #endif
--- Communications/Packet Simulation/test.c
+++ Communications/Packet Simulation/test.c
@@ -3,16 +3,17 @@
 #include "Helpers/header.h"
 
 int main() {
-    header rec;
+    header rec, send;
 
     rec.header_array[0] = 0b00111101;
     rec.header_array[1] = 0b01010101;
 
     rec.header_array[2] = 0b11101010;
     rec.header_array[3] = 0b11111111;
-    rec.header_array[4] = 0b10101010;
-    rec.header_array[5] = 0b10101010;
+    rec.header_array[4] = 0b11111010;
+    rec.header_array[5] = 0b10101111;
 
+    /* Unpack Header Information */
     rec.version_number = get_version_number(rec.header_array);
     rec.packet_type = get_packet_type(rec.header_array);
     rec.sec_header_flag = get_sec_header_flag(rec.header_array);
@@ -28,7 +29,32 @@ int main() {
 
     printf("Sequence flag: %u\n",rec.sequence_flag);
     printf("Sequence count: %u\n",rec.sequence_count);
-    printf("Data length: %u\n",rec.data_length);
+    printf("Data length: %u\n\n",rec.data_length);
+
+    /* Package Header Information */
+    send.header_array[0] = 0;
+    send.version_number = rec.version_number;
+    send.packet_type = rec.packet_type;
+    send.sec_header_flag = rec.sec_header_flag;
+    send.APID = rec.APID;
+    send.sequence_flag = rec.sequence_flag;
+    send.sequence_count = rec.sequence_count;
+    send.data_length = rec.data_length;
+
+    set_version_number(send.version_number, send.header_array);
+    set_packet_type(send.packet_type, send.header_array);
+    set_sec_header_flag(send.sec_header_flag, send.header_array);
+    set_APID(send.APID, send.header_array);
+    set_sequence_flag(send.sequence_flag, send.header_array);
+    set_sequence_count(send.sequence_count, send.header_array);
+    set_data_length(send.data_length, send.header_array);
+
+    printf("Header array - byte 0: %u\n", send.header_array[0]);
+    printf("Header array - byte 1: %u\n", send.header_array[1]);
+    printf("Header array - byte 2: %u\n", send.header_array[2]);
+    printf("Header array - byte 3: %u\n", send.header_array[3]);
+    printf("Header array - byte 4: %u\n", send.header_array[4]);
+    printf("Header array - byte 5: %u\n", send.header_array[5]);
 
     return 0;
 }
